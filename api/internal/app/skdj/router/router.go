b00fc1640e55c5f395199a09aa9459fdd532561a
milopeng <milopeng@tencent.com>
2023-07-18 19:54:19 +0800

    skdj

---
+++ api/internal/app/skdj/router/router.go
@@ -0,0 +1,36 @@
+/*
+* @desc:后台路由
+* @company:舒可到家健康管理有限公司
+* @Author: pmf
+* @Date:   2022/2/18 17:34
+ */
+
+package router
+
+import (
+	"context"
+
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/tiger1103/gfast/v3/internal/app/system/service"
+	"github.com/tiger1103/gfast/v3/library/libRouter"
+)
+
+var R = new(Router)
+
+type Router struct{}
+
+func (router *Router) BindController(ctx context.Context, group *ghttp.RouterGroup) {
+	group.Group("/skdj", func(group *ghttp.RouterGroup) {
+		// 登录验证拦截
+		service.GfToken().Middleware(group)
+		// context拦截器
+		group.Middleware(service.Middleware().Ctx, service.Middleware().Auth)
+		// 后台操作日志记录
+		group.Hook("/*", ghttp.HookAfterOutput, service.OperateLog().OperationLog)
+		group.Bind()
+		// 自动绑定定义的控制器
+		if err := libRouter.RouterAutoBind(ctx, router, group); err != nil {
+			panic(err)
+		}
+	})
+}
--- api/internal/router/router.go
+++ api/internal/router/router.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/gogf/gf/v2/net/ghttp"
 	commonRouter "github.com/tiger1103/gfast/v3/internal/app/common/router"
-	commonService "github.com/tiger1103/gfast/v3/internal/app/common/service"
+	skRouter "github.com/tiger1103/gfast/v3/internal/app/skdj/router"
 	systemRouter "github.com/tiger1103/gfast/v3/internal/app/system/router"
 	"github.com/tiger1103/gfast/v3/library/libRouter"
 )
@@ -24,12 +24,14 @@ type Router struct{}
 func (router *Router) BindController(ctx context.Context, group *ghttp.RouterGroup) {
 	group.Group("/api/v1", func(group *ghttp.RouterGroup) {
 		// 跨域处理，安全起见正式环境请注释该行
-		group.Middleware(commonService.Middleware().MiddlewareCORS)
+		// group.Middleware(commonService.Middleware().MiddlewareCORS)
 		group.Middleware(ghttp.MiddlewareHandlerResponse)
 		// 绑定后台路由
 		systemRouter.R.BindController(ctx, group)
 		// 绑定公共路由
 		commonRouter.R.BindController(ctx, group)
+		// 绑定舒可路由
+		skRouter.R.BindController(ctx, group)
 		// 自动绑定定义的模块
 		if err := libRouter.RouterAutoBind(ctx, router, group); err != nil {
 			panic(err)
